71f809dce2c1a34c240b7d98944739174c0da570
Adriel Mendes <adriel.amendes12@gmail.com>
2022-10-22 02:46:52 -0300

    fix: add isLoading state and reducers

--- libs/shared/app/data-access/src/lib/todo-list.reducer.ts
+++ libs/shared/app/data-access/src/lib/todo-list.reducer.ts
@@ -6,29 +6,57 @@ export const todoListFeatureKey = 'todoList';
 
 export interface State {
   taskList: Task[];
+  isLoading: boolean;
 }
 
 export const initialState: State = {
   taskList: [],
+  isLoading: false,
 };
 
 export const reducer = createReducer(
   initialState,
-  on(TodoListActions.loadTodoList, (state) => state),
+  on(TodoListActions.loadTodoList, (state) => ({
+    ...state,
+    isLoading: true
+  })),
   on(TodoListActions.loadTodoListSuccess, (state, { tasks }) => ({
     ...state,
     taskList: tasks,
+    isLoading: false
   })),
+  on(TodoListActions.loadTodoListError, (state) => ({
+    ...state,
+    isLoading: false
+  })),
+  on(TodoListActions.createTodoList, (state) => ({ ...state, isLoading: true })),
   on(TodoListActions.createTodoListSuccess, (state, { task }) => ({
     ...state,
-    taskList: state.taskList.concat(task),
+    taskList: [...state.taskList, task],
+    isLoading: false
   })),
+  on(TodoListActions.createTodoListError, (state) => ({
+    ...state,
+    isLoading: false
+  })),
+  on(TodoListActions.removeTodoList, (state) => ({ ...state, isLoading: true })),
   on(TodoListActions.removeTodoListSuccess, (state, { taskId }) => ({
     ...state,
     taskList: state.taskList.filter(({ id }) => id !== taskId),
+    isLoading: false
   })),
+  on(TodoListActions.removeTodoListError, (state) => ({
+    ...state,
+    isLoading: false
+  })),
+  on(TodoListActions.updateTodoList, (state) => ({ ...state, isLoading: true })),
   on(TodoListActions.updateTodoListSuccess, (state, { task }) => ({
     ...state,
     taskList: state.taskList.map((t) => (t.id === task.id ? task : t)),
-  }))
+    isLoading: false,
+  })),
+  on(TodoListActions.updateTodoListError, (state) => ({
+    ...state,
+    isLoading: false
+  })),
 );
